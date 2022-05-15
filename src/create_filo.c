2fcc0506ed6e7cfe119eb7b1a4333cc9616589d8
mhaddaou <medo19981602@gmail.com>
2022-05-15 15:44:29 -0400

    philo

--- src/create_filo.c
+++ src/create_filo.c
@@ -6,7 +6,7 @@
 /*   By: mhaddaou <mhaddaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 13:04:22 by mhaddaou          #+#    #+#             */
-/*   Updated: 2022/05/14 14:11:45 by mhaddaou         ###   ########.fr       */
+/*   Updated: 2022/05/14 20:41:28 by mhaddaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ void    fix_data(t_philo *philo)
     philo->p_philo[0].fork.right = philo->info.n_philo;
     
     
-    philo->p_philo[philo->info.n_philo].id = 1;
-    philo->p_philo[philo->info.n_philo].n_t_eat = 0;
-    philo->p_philo[philo->info.n_philo].t_die = 0;
-    philo->p_philo[philo->info.n_philo].fork.left = philo->info.n_philo;
-    philo->p_philo[philo->info.n_philo].fork.right = philo->info.n_philo - 1;
+    philo->p_philo[philo->info.n_philo - 1].id = philo->info.n_philo;
+    philo->p_philo[philo->info.n_philo - 1].n_t_eat = 0;
+    philo->p_philo[philo->info.n_philo - 1].t_die = 0;
+    philo->p_philo[philo->info.n_philo - 1].fork.left = philo->info.n_philo;
+    philo->p_philo[philo->info.n_philo - 1].fork.right = philo->info.n_philo - 1;
 }
 
 void create_filo(t_info *info, t_philo *philo)
@@ -56,14 +56,4 @@ void create_filo(t_info *info, t_philo *philo)
         g++;
     }
     fix_data(philo);
-    int t = 0;
-    while (t < philo->info.n_philo)
-    {
-        printf("id = %d\n", philo->p_philo[t].id);
-        printf("left =  %d\n", philo->p_philo[t].fork.left);
-        printf("right = %d\n", philo->p_philo[t].fork.right);
-        printf("----------------------------------------\n");
-        t++;
-    }
-    
 }
